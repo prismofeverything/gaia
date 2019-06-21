e495f287091213c4aff8087485e9b99d37057134
Ryan Spangler <ryan.spangler@gmail.com>
2019-06-20 23:08:38 -0700

    version

--- project.clj
+++ project.clj
@@ -11,6 +11,6 @@
                  [clj-http "3.7.0"]
                  [ubergraph "0.5.2"]
                  [ophion "0.0.13"]
-                 [sisyphus "0.0.4"]
+                 [sisyphus "0.0.5"]
                  [com.google.guava/guava "23.6-jre"]
                  [org.javaswift/joss "0.9.17"]])
