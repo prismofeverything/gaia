7a6f2f402e28d6093326a0516b8e1289afec243e
Ryan Spangler <ryan.spangler@gmail.com>
2019-07-11 21:45:49 -0700

    sisyphus version

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject gaia "0.0.9"
+(defproject gaia "0.0.10"
   :description "regenerating dependency network"
   :url "http://github.com/prismofeverything/gaia"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :main gaia.core
+  :pedantic? false
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.5"]
                  [com.taoensso/timbre "4.8.0"]
@@ -13,6 +14,6 @@
                  [ubergraph "0.5.2"]
                  [protograph "0.0.19"]
                  [polaris "0.0.19"]
-                 [sisyphus "0.0.5"]
+                 [sisyphus "0.0.6"]
                  [com.google.guava/guava "23.6-jre"]
                  [org.javaswift/joss "0.9.17"]])
