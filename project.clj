247037196f14e8e5ae9a21f0ab737487cb3ea4bc
Ryan Spangler <spanglry@stanford.edu>
2019-08-16 13:09:30 -0700

    sisyphus version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gaia "0.0.14"
+(defproject gaia "0.0.15"
   :description "regenerating dependency network"
   :url "http://github.com/prismofeverything/gaia"
   :license {:name "MIT"
@@ -13,6 +13,6 @@
                  [ubergraph "0.5.2"]
                  [protograph "0.0.19"]
                  [polaris "0.0.19"]
-                 [sisyphus "0.0.14"]
+                 [sisyphus "0.0.15"]
                  [com.google.guava/guava "23.6-jre"]
                  [org.javaswift/joss "0.9.17"]])
